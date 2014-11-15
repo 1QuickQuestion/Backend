2123a0599929c01cf1dd6529a0986f669f935e7d
Marjorie Cécile Hanafi <mhanafi@spendvision.com>
2014-11-16 09:23:40 +1300

    Added shortUrl and qrCode on post before creating

--- Web/api/models/Surveys.js
+++ Web/api/models/Surveys.js
@@ -13,8 +13,20 @@ module.exports = {
         description: 'string',
         location: 'string',
         shortUrl: 'string',
+        qrCode : 'string',
         questions: { collection: 'questions' },
         responses : { collection : 'responses'}
-  }
+    },
+    // Lifecycle Callbacks
+    beforeCreate: function (values, cb) {
+        var bitlyApi = require("node-bitlyapi");
+        var bitly = new bitlyApi();
+        bitly.setAccessToken("2bf203037bb7b0c89d1eb6768b9646551d25d49f");
+        bitly.shortenLink("http://xignal.co/id/" + values.id, function (err, results) {
+            values.shortUrl = JSON.parse(results).data.url;
+            values.qrCode = "https://chart.googleapis.com/chart?chs=150x150&amp;cht=qr&amp;chl=" + values.shortUrl;
+            cb();
+        });
+    }
 };
 
