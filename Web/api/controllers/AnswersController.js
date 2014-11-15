683ec7c02ad6c90fc7de4d888b4da76bb0e1c6e7
Daniel Larsen <daniel@larsen.net.nz>
2014-11-15 13:17:36 +1300

    Data access, questions and answers apis

---
+++ Web/api/controllers/AnswersController.js
@@ -0,0 +1,11 @@
+/**
+ * AnswersController
+ *
+ * @description :: Server-side logic for managing answers
+ * @help        :: See http://links.sailsjs.org/docs/controllers
+ */
+
+module.exports = {
+	
+};
+
