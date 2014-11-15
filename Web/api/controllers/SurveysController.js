cc6174907a823bbc7c187016d497977a774136e9
Marjorie Cécile Hanafi <mhanafi@spendvision.com>
2014-11-15 16:15:55 +1300

    Added api for Surveys, Questions and Responses

--- Web/api/controllers/SurveysController.js
+++ Web/api/controllers/SurveysController.js
@@ -1,7 +1,7 @@
 /**
- * AnswersController
+ * SurveysController
  *
- * @description :: Server-side logic for managing answers
+ * @description :: Server-side logic for managing surveys
  * @help        :: See http://links.sailsjs.org/docs/controllers
  */
 
